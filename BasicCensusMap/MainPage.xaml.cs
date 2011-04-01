3346bc82092f751258f753e660efcec32231cc48
Ryan Resella <ryan@codeforamerica.org>
2011-04-01 11:14:34 -0700

    added basic info window for data

--- BasicCensusMap/MainPage.xaml.cs
+++ BasicCensusMap/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using ESRI.ArcGIS.Client;
+using ESRI.ArcGIS.Client.Toolkit;
 using ESRI.ArcGIS.Client.Geometry;
 using ESRI.ArcGIS.Client.Tasks;
 
@@ -162,6 +163,49 @@ namespace BasicCensusMap
         {
             MessageBox.Show("Locator service failed: " + e.Error);
         }
+
+        private void MyMap_MouseClick(object sender, ESRI.ArcGIS.Client.Map.MouseEventArgs e)
+        {
+            FeatureLayer featureLayer = MyMap.Layers["MyFeatureLayer"] as FeatureLayer;
+            System.Windows.Point screenPnt = MyMap.MapToScreen(e.MapPoint);
+
+            // Account for difference between Map and application origin
+            GeneralTransform generalTransform = MyMap.TransformToVisual(Application.Current.RootVisual);
+            System.Windows.Point transformScreenPnt = generalTransform.Transform(screenPnt);
+
+            IEnumerable<Graphic> selected =
+                featureLayer.FindGraphicsInHostCoordinates(transformScreenPnt);
+
+            foreach (Graphic g in selected)
+            {
+
+                MyInfoWindow.Anchor = e.MapPoint;
+                MyInfoWindow.IsOpen = true;
+                //Since a ContentTemplate is defined, Content will define the DataContext for the ContentTemplate
+                MyInfoWindow.Content = g.Attributes;
+                return;
+            }
+
+            InfoWindow window = new InfoWindow()
+            {
+                Anchor = e.MapPoint,
+                Map = MyMap,
+                IsOpen = true,
+                ContentTemplate = LayoutRoot.Resources["LocationInfoWindowTemplate"] as System.Windows.DataTemplate,
+                //Since a ContentTemplate is defined, Content will define the DataContext for the ContentTemplate
+                Content = e.MapPoint
+            };
+            LayoutRoot.Children.Add(window);
+        }
+        
+
+        private void MyInfoWindow_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MyInfoWindow.IsOpen = false;
+        }
+
+
+
     }
 
 
